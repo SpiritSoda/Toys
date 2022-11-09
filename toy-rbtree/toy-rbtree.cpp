52cffc7f85cc467c5b33f1cc7d79f1ca00f8600b
SpiritSoda <47811441+SpiritSoda@users.noreply.github.com>
2022-11-09 13:02:43 +0800

    [Bug Fixed | Toy RB Tree]
    fixed an issue in tree rotate which will change null node's parent pointer

--- toy-rbtree/toy-rbtree.cpp
+++ toy-rbtree/toy-rbtree.cpp
@@ -9,6 +9,7 @@ enum RBColor{
 	RED = 0,
 	BLACK = 1
 };
+// RB tree node
 class RBNode{
 protected:
 	RBColor color;
@@ -19,6 +20,7 @@ protected:
 	
 	friend class RBTree;
 public:
+	// null node
 	static RBNode* null;
 	RBNode(int value): color(RED), key(value), left(null), right(null), parent(null){}
 	RBNode(){
@@ -35,14 +37,20 @@ public:
 			return "black";
 		return "unknown";
 	}
+	int get_key(){
+		return key;
+	} 
 };
 RBNode* RBNode::null = new RBNode();
 RBNode* null = RBNode::null;
 
 class RBTree{
 private:
+    // root pointer
 	RBNode* root; 	
 	
+    // some private operations like rotation and attribute fixer
+	// output function
 	void preorder(RBNode* node, ofstream& fout){
 		if(node == null)
 			return;
@@ -64,16 +72,19 @@ private:
 		postorder(node->right, fout);
 		fout << node->key << ",\t" << node->get_color() << endl;
 	}
+	// get minimum value in a subtree
 	RBNode* tree_minimum(RBNode* node){
 		RBNode* scan = node;
 		while(scan->left != null)
 			scan = scan->left;
 		return scan;
 	}
+	// rotate
 	void rotate_left(RBNode* x){
 		RBNode* y = x->right;
 		x->right = y->left;
-		y->left->parent = x;
+		if(y->left != null)
+			y->left->parent = x;
 		y->parent = x->parent;
 		if(x->parent == null)
 			root = y;
@@ -87,7 +98,8 @@ private:
 	void rotate_right(RBNode* x){
 		RBNode* y = x->left;
 		x->left = y->right;
-		y->right->parent = x;
+		if(y->right != null)
+			y->right->parent = x;
 		y->parent = x->parent;
 		if(x->parent == null)
 			root = y;
@@ -97,8 +109,8 @@ private:
 			x->parent->right = y;
 		y->right = x;
 		x->parent = y;
-			
 	}
+	// tree node transplant, will be used in rb_delete
 	void rb_transplant(RBNode* u, RBNode* v){
 		if(u->parent == null)
 			root = v;
@@ -108,6 +120,7 @@ private:
 			u->parent->right= v;
 		v->parent = u->parent;
 	}
+	// insert fixup
 	void rb_insert_fixup(RBNode* z){
 		while(z->parent->color == RED){
 			if(z->parent == z->parent->parent->left){
@@ -166,12 +179,14 @@ private:
 		}
 		root->color = BLACK;
 	}
+	// delete fixup
 	void rb_delete_fixup(RBNode* x){
 		while(x != root && x->color == BLACK){
 			if(x == x->parent->left){
 				RBNode* w = x->parent->right;
 				if(w->color == RED){
 					// case 1
+					cout << "delete case 1" << endl;
 					w->color = BLACK;
 					x->parent->color = RED;
 					rotate_left(x->parent);
@@ -179,18 +194,22 @@ private:
 				}
 				if(w->left->color == BLACK && w->right->color == BLACK){
 					// case 2
+					cout << "delete case 2" << endl;
 					w->color = RED;
 					x = x->parent;
 				}
 				else{	
 					if(w->right->color == BLACK){
 						// case 3
+						// transfer to case 4
+						cout << "delete case 3" << endl;
 						w->left->color = BLACK;
 						w->color = RED;
 						rotate_right(w);
 						w = x->parent->right;
 					}
 					// case 4
+					cout << "delete case 4" << endl;
 					w->color = x->parent->color;
 					x->parent->color = BLACK;
 					w->right->color = BLACK;
@@ -202,6 +221,7 @@ private:
 				RBNode* w = x->parent->left;
 				if(w->color == RED){
 					// case 5
+					cout << "delete case 5" << endl;
 					w->color = BLACK;
 					x->parent->color = RED;
 					rotate_right(x->parent);
@@ -209,18 +229,22 @@ private:
 				}
 				if(w->left->color == BLACK && w->right->color == BLACK){
 					// case 6
+					cout << "delete case 6" << endl;
 					w->color = RED;
 					x = x->parent;
 				}
 				else{	
 					if(w->left->color == BLACK){
 						// case 7
+						// transfer to case 8
+						cout << "delete case 7" << endl;
 						w->right->color = BLACK;
 						w->color = RED;
 						rotate_left(w);
 						w = x->parent->left;
 					}
 					// case 8
+					cout << "delete case 8" << endl;
 					w->color = x->parent->color;
 					x->parent->color = BLACK;
 					w->left->color = BLACK;
@@ -231,17 +255,33 @@ private:
 		}
 		x->color = BLACK;
 	}
+	// check subtree's RB attributes, returns validation and black height
 	pair<bool, int> rb_check(RBNode* node){
+    	// reach leaf node, return valid tree with black height 0
 		if(node == null){
 			return make_pair(true, 0);
 		}
+    	// check if child's subtree is valid and get their black height 
 		pair<bool, int> res1 = rb_check(node->left), res2 = rb_check(node->right);
+		// if one of the subtrees is invalid, return invalid
 		if(!res1.first || !res2.first)
 			return make_pair(false, -1);
-		if(res1.second != res2.second)
+		// attribute 5: if subtrees' black height is not equal, return invalid
+		if(res1.second != res2.second){
+			cout << "RB Error::subtree's black height not equal: " << node->key << endl;
 			return make_pair(false, -1);
-		if(node->color == RED && (node->left->color != BLACK || node->right->color != BLACK))
+		}
+    	// attribute 4: if current node is red, its child nodes must be black
+		if(node->color == RED && (node->left->color != BLACK || node->right->color != BLACK)){
+			cout << "RB Error::red node's child is not black: " << node->key << endl;
 			return make_pair(false, -1);
+		}
+    	// BST attribute verification
+		if(!((node->left == null || node->key > node->left->key) && (node->right == null || node->key < node->right->key))){
+			cout << "RB Error::invalid BST: " << node->key << endl;
+			return make_pair(false, -1);
+		}
+    	// all verification passed, return valid and update black height
 		return make_pair(true, res1.second + (node->color == BLACK ? 1 : 0));
 	}
 public:
@@ -249,6 +289,7 @@ public:
 		root = null;
 		root->parent = null;
 	}
+	// insert a key into rb tree
 	void rb_insert(int value){
 		RBNode* node = new RBNode(value);
 		if(root == null)
@@ -278,6 +319,7 @@ public:
 		}
 		rb_insert_fixup(node);
 	}
+	// search given key in rb tree
 	RBNode* rb_search(int key){
 		RBNode* scan = root;
 		while(scan != null){
@@ -290,8 +332,9 @@ public:
 					scan = scan->right;
 			}
 		}
-		return null;
+		return NULL;
 	}
+	// delete a node with given key in rb tree
 	void rb_delete(int key){
 		RBNode* z = rb_search(key);
 		if(z == null)
@@ -327,6 +370,7 @@ public:
 		if(y_origin_color == BLACK)
 			rb_delete_fixup(x);
 	}
+	// output
 	void preorder(string out){
 		ofstream fout(out);
 		preorder(root, fout);
@@ -347,14 +391,24 @@ public:
 			RBNode* node = q.front();
 			q.pop();
 			
-			fout << node->key << ",\t" << node->get_color() << endl;
+			int layer = 1;
+			RBNode* scan = node->parent;
+			while(scan != null){
+				scan = scan->parent;
+				layer ++;
+			}
+			fout << node->key << ",\t" << node->get_color() << ",\t\t" << layer << endl;
 			if(node->left != null)
 				q.push(node->left);
 			if(node->right != null)
 				q.push(node->right);
 		}
 	}
+	// verify rb tree validation
 	bool rb_verify(){
+    	// atrribute 1: limited by the enum type RBColor, must be satisfied
+    	// attribute 3: leaf node is 'null', which is black, satisfied
+    	// attribute 2: if root node is not black return invalid
 		if(root->color != BLACK)
 			return false;
 		return rb_check(root).first;
@@ -374,6 +428,27 @@ int main(){
 	while(ss >> key){
 		rbtree.rb_insert(key);
 	}
+	cout << endl;
+	rbtree.rb_delete(14);
+	rbtree.rb_delete(13);
+	rbtree.rb_delete(3);
+	rbtree.rb_delete(4);
+	rbtree.rb_delete(9);
+	rbtree.rb_delete(5);
+	rbtree.rb_delete(19);
+	rbtree.rb_delete(0);
+	rbtree.rb_delete(17);
+	rbtree.rb_delete(8);
+	rbtree.rb_delete(15);
+	rbtree.rb_delete(10);
+	rbtree.rb_delete(11);
+	rbtree.rb_delete(18);
+	rbtree.rb_delete(12);
+	rbtree.rb_delete(16);
+	rbtree.rb_delete(7);
+	rbtree.rb_delete(2);
+	rbtree.rb_delete(1);
+	rbtree.rb_delete(6);
 	cout << endl;
 	cout << (rbtree.rb_verify() ? "valid RB tree" : "invalid RB tree");
 	rbtree.preorder("NLR.txt");
